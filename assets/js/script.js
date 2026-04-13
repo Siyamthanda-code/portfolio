d9b977159dbcc981216726d46f539307e5291c5d
Siyamthanda Amahle Buthelezi <siyamthanda917@gmail.com>
2026-04-13 13:50:14 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analysis", "Data Engineering", "Database Admnistration", "Software Development"],
+    strings: ["Data Analysis", "Data Engineering", "Database Admnistration", "Web Development"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
